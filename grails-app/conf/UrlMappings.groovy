63dbe36bd3119c8cdf26d8407c5289e412b1a416
Nico Sallembien <nsallembien@gmail.com>
2014-03-18 17:15:47 -0700

    Testing adding new files

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -3,6 +3,7 @@ class UrlMappings {
 	static mappings = {
         "/dataset/$id/chunk/$chunk"(controller: "dataset", action: "nextChunk")
         "/dataset/$id/chunk.json/$chunk"(controller: "dataset", action: "nextChunk_json")
+        "/$controller/list"(action: "index") 
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
